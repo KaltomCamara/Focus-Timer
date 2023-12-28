8b09ab5e2d3c1129364a350c2cd09907e16dc7d4
KaltomCamara <kaltomcamara@hotmail.com>
2023-12-28 17:40:04 -0300

    first commit

---
+++ src/FocusTimer/actions.js
@@ -0,0 +1,17 @@
+import state from "./state.js"
+
+export function toggleRunning(){
+    state.isRunning = document.documentElement.classList.toggle("running")
+}
+
+export function reset(){
+    document.documentElement.classList.remove("running")
+}
+
+export function set(){
+
+}
+
+export function toggleMusic(){
+    state.isMute = document.documentElement.classList.toggle("music-on")
+}
