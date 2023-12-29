bae575bbae83bbd452eb7855cecca329d08198f7
KaltomCamara <kaltomcamara@hotmail.com>
2023-12-29 11:27:15 -0300

    Adicionando som

--- src/FocusTimer/events.js
+++ src/FocusTimer/events.js
@@ -1,8 +1,11 @@
+import state from "./state.js"
 import {controls} from "./elements.js"
 import * as actions from "./actions.js"
+import * as el from "./elements.js"
+import { updateDisplay } from "./timer.js"
 
 export function registerControls(){
-    controls.addEvenListener("click", (event) => {
+    controls.addEventListener("click", (event) => {
         const action = event.target.dataset.action
         
         if(typeof actions[action] != "function"){
@@ -11,4 +14,23 @@ export function registerControls(){
         
         actions[action]()
     })
+} 
+
+export function setMinutes(){
+    el.minutes.addEventListener("focus", () => {
+        el.minutes.textContent = ""
+    })
+
+    el.minutes.onkeypress = (event) => /\d/.test(event.key)
+
+    el.minutes.addEventListener("blur", (event) => {
+        let time = event.currentTarget.textContent
+        time = time >60 ? 60 : time
+
+        state.minutes = time
+        state.seconds = 0
+
+        updateDisplay()
+        el.minutes.removeAttribute("contenteditable")
+        })
 }
